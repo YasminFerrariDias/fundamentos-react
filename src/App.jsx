cab0b65b10104b4094080aadeef7194c8fd36e5f
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-03-27 11:12:30 -0300

    Iniciando o componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
+import { SideBar } from './components/SideBar'
 import "./global.css"
+import styles from './App.module.css'
 
 export default function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="John Doe" 
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quo sed provident quasi ut minus eos mollitia nemo rerum delectus porro autem culpa repellat deleniti beatae, minima temporibus asperiores dolor."
-      />
-      <Post 
-        author="MarceloLocky" 
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-      />
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          <Post
+            author="Yasmin Dias"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate."
+          />
+          <Post
+            author="John Doe"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quo sed provident quasi ut minus eos mollitia nemo rerum delectus porro autem culpa repellat deleniti beatae, minima temporibus asperiores dolor."
+          />
+        </main>
+      </div>
     </div>
   )
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -2,7 +2,7 @@ export function Post(props) {
   return (
     <>
       <strong>{props.author}</strong>
-      <p>{props.description}</p>
+      <p>{props.content}</p>
     </>
   );
 }
