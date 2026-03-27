1c11c93b7cb041067e36fee56e0103fd2f20ddc3
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-03-27 10:05:11 -0300

    Conhecendo o css modulo-

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
-
+import "./styles.css"
 
 export default function App() {
   return (
     <div>
+      <Header />
       <Post 
         author="John Doe" 
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quo sed provident quasi ut minus eos mollitia nemo rerum delectus porro autem culpa repellat deleniti beatae, minima temporibus asperiores dolor."
