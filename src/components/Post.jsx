c7455420c35f65481c278f4fc1b6b8f66aa490b1
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-03-27 15:45:47 -0300

    Estilização do post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -26,10 +26,11 @@ export function Post() {
           no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
         <p>
-          👉<a href="">jane.design/doctorcare</a>
+          <a href="">jane.design/doctorcare</a>
         </p>
         <p>
-          <a href="">#novoprojeto #nlw #rocketseat</a>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
         </p>
       </div>
     </article>
