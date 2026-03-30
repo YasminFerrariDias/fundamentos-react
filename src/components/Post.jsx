5134d97bd59a061bcaad00d23b1935b48c434ae0
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-03-30 11:58:48 -0300

    Utilizando Estado - useState

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,24 +1,36 @@
 import { format, formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { useState } from "react";
 
 import { Avatar } from "./avatar";
 import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
 
+
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    1,
+    2
+  ]);
+
   const publishedDateFormatted = format(
     publishedAt,
     "d 'de' LLLL 'às' HH:mm'h'",
     {
       locale: ptBR,
-      addSuffix: true,
     },
   );
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
+    addSuffix: true,
   });
+
+  function handleCreateNewComment(event) {
+    event.preventDefault();
+    setComments([...comments, comments.length + 1]);
+  }
 
   return (
     <article className={styles.post}>
@@ -40,17 +52,22 @@ export function Post({ author, publishedAt, content }) {
       </header>
 
       <div className={styles.content}>
-        {content.map((line) => {
-          if (line.type == "paragraph") {
-            return <p>{line.content}</p>;
-          } else if (line.type == "link") {
-            return <p><a href="">{line.content}</a></p>;
+        {content.map((line, index) => {
+          if (line.type === "paragraph") {
+            return <p key={index}>{line.content}</p>;
+          }
 
+          if (line.type === "link") {
+            return (
+              <p key={index}>
+                <a href="">{line.content}</a>
+              </p>
+            );
           }
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea placeholder="Deixe um comentário" />
 
@@ -60,9 +77,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment key={comment} />;
+        })}
       </div>
     </article>
   );
