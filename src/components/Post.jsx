968a6e6c50564686f8fb791a0e09c7bb5d2a804d
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-03-30 14:36:21 -0300

    Validando o formulário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -10,7 +10,9 @@ import styles from "./Post.module.css";
 export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState(["Post muito bacana, hein?!"]);
 
-  const [newComentText, setNewCommentText] = useState("");
+  const [newCommentText, setNewCommentText] = useState("");
+
+  console.log(newCommentText)
 
   const publishedDateFormatted = format(
     publishedAt,
@@ -33,7 +35,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange(event) {
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório!')
   }
 
   function deleteComment(commentToDelete) {
@@ -43,6 +50,8 @@ export function Post({ author, publishedAt, content }) {
 
     setComments(commentsWithoutDeletedOne)
   }
+
+  const isNewCommentEmpty = newCommentText.length == 0;
 
   return (
     <article className={styles.post}>
@@ -78,12 +87,14 @@ export function Post({ author, publishedAt, content }) {
         <textarea 
         name="comment" 
         placeholder="Deixe um comentário" 
-        value={newComentText}
+        value={newCommentText}
         onChange={handleNewCommentChange}
+        onInvalid={handleNewCommentInvalid}
+        required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty} >Publicar</button>
         </footer>
       </form>
 
